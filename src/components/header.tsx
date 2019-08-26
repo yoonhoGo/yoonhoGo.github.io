1b690bbb28e67cc1e7e6de7cd103f06a79cd5631
Yoonho Go <enoch@erion.kr>
2019-08-26 18:12:58 +0900

    Apply styled-components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
 const Header = ({ siteTitle }: { siteTitle: string }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <HeaderMargin>
+    <Padding>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </header>
+    </Padding>
+  </HeaderMargin>
 )
 
 Header.propTypes = {
@@ -40,3 +29,13 @@ Header.defaultProps = {
 }
 
 export default Header
+
+const HeaderMargin = styled.header`
+  margin-bottom: 1.45rem;
+`
+
+const Padding = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import styled from "styled-components"
 
 const Layout = ({ children }: { children: any }) => {
   const data = useStaticQuery(graphql`
@@ -26,21 +27,14 @@ const Layout = ({ children }: { children: any }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <Body>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Body>
     </>
   )
 }
@@ -50,3 +44,10 @@ Layout.propTypes = {
 }
 
 export default Layout
+
+const Body = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`

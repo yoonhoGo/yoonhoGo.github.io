67ef37e12173237cb1d0748c334fcfdec58c1559
Yoonho Go <enoch@erion.kr>
2019-11-28 15:15:12 +0900

    Add gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,12 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`, `/calling-card`],
+      },
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-sass`,
